9af24d452bd58a9b32385ebf9c86f5a04165f526
kailous <kailous@live.cn>
2023-02-20 21:21:21 +0800

    初始化

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # 设定水印图片的路径
-watermark="https://kailous.github.io/watermark/xiaome.png"
+watermark="../xiaome.png"
 # 遍历当前目录下所有的jpg文件
 cd img
 for file in *.jpg; do
   # 给图片加水印
-  composite -dissolve 30% -tile ${watermark} "${file}" "../out/watermark_${file}"
+  composite -dissolve 50% -tile ${watermark} "${file}" "../out/watermark_${file}"
   echo ${file} 添加成功
 done
